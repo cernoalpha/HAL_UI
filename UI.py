d43a3f3ca923580b18fe13ac14d98e66da2b9be5
cernoalpha <mirshahzadmubeen1@gmail.com>
2024-05-10 16:58:41 +0530

    ne commit

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 from customtkinter import CTk, CTkFrame, CTkLabel, CTkButton, CTkFont
-from relay_control import relay_on, relay_off
+from relay_control import BitBangDevice,relay_on, relay_off
+from functools import partial
 
 class App(CTk):
     def __init__(self):
@@ -12,6 +13,14 @@ class App(CTk):
         screen_height = self.winfo_screenheight()
         frame3_width = int(screen_width * 0.75)  
         frame3_height = int(screen_height * 0.8)
+
+        try:
+            self.bb = BitBangDevice('AC00LH65')
+            self.bb.direction = 0x0F
+            self.relay_connected = True
+        except Exception as e:
+            print("Error:", e)
+            self.relay_connected = False
 
         self.frame0 = CTkFrame(self, width=300)
         self.frame0.grid(row=0, column=0, padx=20, pady=20)
@@ -34,17 +43,29 @@ class App(CTk):
         self.label2 = CTkLabel(self.frame2, text="Relay")
         self.label2.grid(row=0, column=0, pady=(10,0),sticky="ew", columnspan=3)
 
-        self.button3 = CTkButton(self.frame2, text="LED1", command=self.led1_event)
-        self.button3.grid(row=1, column=0, pady=(20, 10),padx=30, columnspan=3, sticky="ew")
+        self.led_buttons = []
 
-        self.button4 = CTkButton(self.frame2, text="LED2", command=self.led2_event)
-        self.button4.grid(row=2, column=0, pady=10, padx=30 ,columnspan=3, sticky="ew")
-
-        self.button5 = CTkButton(self.frame2, text="LED3",  command=self.led3_event)
-        self.button5.grid(row=3, column=0, pady=(10, 20),padx=30 ,columnspan=3, sticky="ew")
+        for i in range(1, 4):
+            button = CTkButton(self.frame2, text=f"LED{i}", command=partial(self.led_event, i))
+            button.grid(row=i, column=0, pady=(20, 10), padx=30, columnspan=3, sticky="ew")
+            self.led_buttons.append(button)
 
         self.frame3 = CTkFrame(self, width=frame3_width, height=frame3_height)
         self.frame3.grid(row=0, column=2, padx=20, pady=(50,20))
+
+        self.led_states = [False, False, False]
+
+        if not self.relay_connected:
+            self.label2.configure(text_color="red")
+            print("Label2 red")
+
+            for button in self.led_buttons:
+                button.configure(state="disabled")
+
+        else:
+            self.label2.configure(text_color="white")
+            for button in self.led_buttons:
+                button.config(state="normal")
 
     def capture_event(self):
         print("Capture button clicked")
@@ -52,14 +73,15 @@ class App(CTk):
     def live_event(self):
         print("Live button clicked")
 
-    def led1_event(self):
-        print("LED1 button clicked")
-
-    def led2_event(self):
-        print("LED2 button clicked")
-
-    def led3_event(self):
-        print("LED3 button clicked")
+    def led_event(self, index):
+        print(f"LED{index} button clicked")
+        self.led_states[index - 1] = not self.led_states[index - 1]
+        if self.led_states[index - 1]:
+            self.led_buttons[index - 1].configure(fg_color="green")  
+            relay_on(index, self.bb)  
+        else:
+            self.led_buttons[index - 1].configure(fg_color='red')  
+            relay_off(index, self.bb) 
 
 
 
